04b3808bf4c5e9bb05b61f893a2ea798706ac94f
kakikukeko21 <fightingspirit763@outlook.jp>
2022-06-27 17:23:08 +0900

    カリキュラム8-4

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/posts', 'PostController@store');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
+Route::get('/posts/{post}', 'PostController@show');
+
+
